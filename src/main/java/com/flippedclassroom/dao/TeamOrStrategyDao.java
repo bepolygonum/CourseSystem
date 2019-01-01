267a364b03710896e2b16242c68fa4b39f5facf9
Luobupi <CredenceLuo@outlook.com>
2019-01-01 22:25:17 +0800

    commit

--- src/main/java/com/flippedclassroom/dao/TeamOrStrategyDao.java
+++ src/main/java/com/flippedclassroom/dao/TeamOrStrategyDao.java
@@ -1,11 +1,21 @@
 package com.flippedclassroom.dao;
 
+import com.flippedclassroom.entity.TeamAndStrategy;
+import com.flippedclassroom.entity.TeamOrStrategy;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author DELL
  */
+@Repository
 public interface TeamOrStrategyDao {
+
     void createTeamOrStrategy(@Param("0") int id, @Param("1") String strategyName, @Param("2") int strategyId);
+
     int selectTeamOrStrategyMaxId();
+
+    List<TeamOrStrategy> getTeamOrStrategyByid(@Param("0") int strategyId);
 }
--- src/main/java/com/flippedclassroom/entity/MemberLimitStrategy.java
+++ src/main/java/com/flippedclassroom/entity/MemberLimitStrategy.java
@@ -1,15 +1,17 @@
 package com.flippedclassroom.entity;
 
 
-public class MemberLimitStrategy{
+public class MemberLimitStrategy {
     private int id;
     private int minMember;
     private int maxMember;
-    public boolean isValid(int number){
-        if(number>minMember&& number<=maxMember)
+
+    public boolean isValid(int number) {
+        if (number >= minMember && number <= maxMember)
             return true;
         else
             return false;
+    }
 
     public int getId() {
         return id;
